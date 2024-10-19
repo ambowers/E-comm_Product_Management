cde1d9e459a2f1e6d86bd57446018c02ced3d4e2
lina <ambowers@usf.edu>
2024-10-19 14:08:31 -0400

    Add event listener to update price on size selection

---
+++ main.js
@@ -0,0 +1,11 @@
+// Task 2 add event listeners for product selection
+
+const productOptions = {
+    geometric: {price: 34.99},
+    sunset:{price:24.99},
+    pearl:{price:29.99},
+    floral:{price:34.99}
+};
+const priceElement = document.getElementById('product-price');
+const colorOptions = document.getElementById('color');
+const purchaseButton = document.getElementById('purchase_button');
